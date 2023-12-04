e13ff7a8938165ecc8ae68285acdfddea276367d
deepak sah <sahdeepak330@gmail.com>
2023-12-04 16:26:08 +0530

    'navigate to dashborad after success login' issue solved with protected routes

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,37 +1,27 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./components/pages/Login";
+import Signup from "./components/pages/Signup";
 import Dashboard from "./components/pages/Dashboard";
-import { getCookie } from "./utils/getCookie";
 import DashboardLayout from "./layout/DashboardLayout";
 import Banners from "./components/pages/Banners";
 import Products from "./components/pages/Products";
 import AddBanner from "./components/pages/AddBanner";
+import ProtectedRoute from "./components/common/ProtectedRoute";
 
 export default function AppRoutes() {
-  return getCookie("token") ? (
-    <Routes>
-      <Route path="/login" element={<Navigate to="/dashboard" />} />
-      <Route path="/signup" element={<Navigate to="/dashboard" />} />
-      <Route path="/" element={<DashboardLayout />}>
-        <Route index element={<Navigate to="/dashboard" />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/banners" element={<Banners />} />
-        <Route path='/add-banner' element={<AddBanner/>}/>
-        <Route path="/products" element={<Products />} />
-      </Route>
-    </Routes>
-  ) : (
-    <Routes>
-      <Route path="/login" element={<Navigate to="/dashboard" />} />
-      <Route path="/signup" element={<Navigate to="/dashboard" />} />
-      <Route path="/" element={<DashboardLayout />}>
-        <Route index element={<Navigate to="/login" />} />
-        <Route path="/dashboard" element={<Login />} />
-        <Route path="/banners" element={<Login />} />
-        <Route path='/add-banner' element={<Login/>}/>
-        <Route path="/products" element={<Login />} />
-      </Route>
+
+  return(
+  <Routes>
+          <Route path='/login' element={<ProtectedRoute shouldLoggedIn={false} component={Login}/>} />
+          <Route path='/signup' element={<ProtectedRoute shouldLoggedIn={false} component={Signup}/>} />
+          <Route path='/' element={<ProtectedRoute shouldLoggedIn={true} component={DashboardLayout}/>}>
+            <Route index element={<Navigate to='/dashboard' />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/banners' element={<Banners />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/add-banner' element={<AddBanner />} />
+          </Route>
     </Routes>
   );
 }
---
+++ src/components/common/ProtectedRoute.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { getCookie } from "../../utils/getCookie";
+
+const ProtectedRoute = (props) => {
+    
+  return (
+    props.shouldLoggedIn?
+         (getCookie('token')) ?
+           (<props.component />)
+        :  (
+            <Navigate
+            to= "/signup"
+            />
+          )
+           :
+           (getCookie('token')) ?
+           (<Navigate
+            to= "/dashboard"
+            />)
+        :  (
+          <props.component /> 
+          ) 
+  );
+};
+
+export default ProtectedRoute;
